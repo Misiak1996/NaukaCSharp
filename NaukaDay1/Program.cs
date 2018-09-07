116791626d51488be82b6e5399024a584db74ddd
Misiak <Misiak@DESKTOP-NGERQUU>
2018-09-07 20:22:16 +0200

    Dodanie komentarzy w formacie XML

--- NaukaDay1/Program.cs
+++ NaukaDay1/Program.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/// <summary>
+/// Komentarz ten dodawany jest przed klasami, metodami.
+/// </summary>
 namespace NaukaDay1
 {
+    /// <summary>
+    /// Klasa zawierajaca metode main start programu
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+             /// <summary>
+             /// Metoda main
+             /// </summary>
+             /// <param name="args"></param>
+        static void main(string[] args)
         {
-            Console.WriteLine("Hello");
+   
         }
     }
 }
